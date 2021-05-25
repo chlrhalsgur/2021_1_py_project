e5ff39ed23f4c5a63e4ca5acd14f28ab7978a03f
chlrhalsgur <chlrhalsgur0209@gmail.com>
2021-05-25 16:45:01 +0900

    restart pc

---
+++ crawling_test1.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from bs4 import Beautifulsoup
+
+
+search = input('')
+url = 'https://1xbet.whoscored.com/Statistics'
+html = urlopen(url).read()
+soup = Beautifulsoup(html,'html.parser')
+
+play_time = 'minsPlayed   '
+
+print(play_time[0])
